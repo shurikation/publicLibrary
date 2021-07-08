efb8709df17d4d62743893239c8a81b1294f442b
shurikation <shurikation@gmail.com>
2021-07-08 22:29:28 +0400

    feat: add save data, test data and clear data methods

--- src/model/Book.js
+++ src/model/Book.js
@@ -16,15 +16,12 @@ Book.add = function (slots) {
 	console.log(`Book ${slots.isbn} created!`);
 };
 
-//   booksString = localStorage["books"];
-//   books = JSON.parse(booksString);
-
 Book.convertRec2Obj = function (bookRow) {
 	const book = new Book(bookRow);
 	return book;
 };
 
-// key: { isbn:"006251587X", title:"Weaving the Web", year:2000 }
+// "006251587X" : { isbn:"006251587X", title:"Weaving the Web", year:2000 }
 
 Book.retrieveAll = function () {
 	var booksString = "";
@@ -46,14 +43,13 @@ Book.retrieveAll = function () {
 };
 
 Book.update = function (slots) {
-	const book = Book.instances[slots.isbn],
-		year = parseInt(slots.year);
+	const book = Book.instances[slots.isbn], year = parseInt(slots.year);
 
 	if (book.title !== slots.title) book.title = slots.title;
 	if (book.year !== year) books.year = year;
+
 	console.log(`Book ${slots.isbn} has been updated!`);
 }
-
 
 Book.destroy = function (isbn) {
 	if (Book.instances[isbn]) {
@@ -64,6 +60,36 @@ Book.destroy = function (isbn) {
 	}
 };
 
+Book.saveAll = function () {
+	var error = false;
+	try {
+		const booksString = JSON.stringify(Book.instances);
+		localStorage["books"] = booksString;
+	} catch (e) {
+		alert("Error when writing to Local Storage\n" + e);
+		error = true;
+	}
+	if (!error) {
+		const nmrOfBooks = Object.keys(Book.instances).length;
+		console.log(`${nmrOfBooks} books saved.`);
+	}
+};
+
+Book.createTestData = function () {
+	Book.instances["006251587X"] = new Book(
+		{ isbn: "006251587X", title: "Weaving the Web", year: 2000 });
+	Book.instances["0465026567"] = new Book(
+		{ isbn: "0465026567", title: "Gödel, Escher, Bach", year: 1999 });
+	Book.instances["0465030793"] = new Book(
+		{ isbn: "0465030793", title: "I Am A Strange Loop", year: 2008 });
+	Book.saveAll();
+};
+
+Book.clearData = function () {
+	if (confirm("Do you really want to delete all book data?")) {
+		localStorage["books"] = "{}";
+	}
+};
 
 
 
